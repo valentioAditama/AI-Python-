9cca4cab9f6961382cc1bddf8db938d1f5ea42d6
valentioaditama <valentioaditama8@gmail.com>
2022-12-09 00:12:49 +0700

    add update AI

--- SkyAI/main.py
+++ SkyAI/main.py
@@ -2,15 +2,18 @@
 import pyttsx3 as outputSound
 from datetime import datetime
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Method
 def speak(command):
 	# engine sound voice
 	engine = outputSound.init()
 	engine.say(command)
 	engine.runAndWait()
-	
 
-
-# Method
 def welcome():
 	# datetime now
 	dateNow = datetime.now()
@@ -19,20 +22,61 @@ def welcome():
 	# say me on time 	
 	if dateNow.hour >= 0 and dateNow.hour <= 10:
 		print('Good Morning, Sir')
+		speak('Good Morning, si')
 	elif dateNow.hour >= 10 and dateNow.hour <= 18:
 		print('Good afternoon, Sir')
+		speak('Good afternoon, Sir')	
 	elif dateNow.hour >= 18 and dateNow.hour <= 21:
 		print('Good Night, Sir')
+		speak('Good Night, Sir')
 	else:
 		print('Good Evening')
+		speak('Good Evening')
 
 	print('Hi Valent!, can i help you?')
 
+def menu():
+	print('1. open google\n2. open youtube\n3.Quit')
+
+def searchGoogle(comamnd):
+	driver = webdriver.Chrome()
+	driver.get('https://www.google.com')
+	driver.find_elements(by=By.NAME, value='q')
+	driver.Send_keys(comamnd)
+	time.sleep(3)
+	driver.find_elements(by=By.NAME, value='btnK')
+	driver.Send_keys(Keys.ENTER)
+	time.sleep(3)
+	driver.close
+	
 
 if __name__ == '__main__':
-	# Method
+	# testing area
+	searchingGoogle = input('Please Search > ')
+	searchGoogle(searchingGoogle)
+
+	# Show My Method 
 	print()
 	welcome()
-
+	
 	while True:
-		speak('hello world')
+		menu()
+		ask = input('choose > ')
+		
+		if ask == "1":
+			print('Opened Google...')
+			time.sleep(3)
+			print('waiting... ')
+			time.sleep(2)
+			searchingGoogle = input('Please Search > ')
+			searchGoogle(searchingGoogle)
+			print('Result Search > ', searchingGoogle)
+
+		elif ask == "2":
+			print("coming soon")
+			break
+		elif ask == "3":
+			print("coming soon")
+			quit(1)
+		else:
+			print('nothing')
