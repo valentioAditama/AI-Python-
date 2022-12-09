e22a27ee0fcf218b5d6be0abe7130c73ff8adf01
valentioaditama <valentioaditama8@gmail.com>
2022-12-09 14:23:56 +0700

    update AI

--- SkyAI/main.py
+++ SkyAI/main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 import time
 
 # Method
@@ -36,47 +37,87 @@ def welcome():
 	print('Hi Valent!, can i help you?')
 
 def menu():
-	print('1. open google\n2. open youtube\n3.Quit')
+	print('1. open google\n2. open youtube\n3. Quit\n4. Prank')
+
+
+# Duration Second Openned Selenium Chrome
+def durationOpened(command):
+	time.sleep(command)
+	print('Duration > ', command, 'Second')
+
 
 def searchGoogle(comamnd):
 	driver = webdriver.Chrome()
 	driver.get('https://www.google.com')
-	driver.find_elements(by=By.NAME, value='q')
-	driver.Send_keys(comamnd)
+	formTextField = driver.find_elements(by=By.NAME, value='q');
+	formTextField[0].send_keys(comamnd)
 	time.sleep(3)
-	driver.find_elements(by=By.NAME, value='btnK')
-	driver.Send_keys(Keys.ENTER)
-	time.sleep(3)
-	driver.close
-	
+	btnSubmit = driver.find_elements(by=By.NAME, value='btnK')
+	btnSubmit[0].send_keys(Keys.ENTER)
+
+	durationSecond = int(input('Duration Second ? > '))
+	durationOpened(durationSecond)
+
+	print('running.....')
+
+def searchYoutube(command):
+	driver = webdriver.Chrome()
+	driver.get('https://www.youtube.com')
+	formTextField = driver.find_elements(by=By.NAME, value='search_query')
+	formTextField[0].send_keys('python is')
+	btnSubmit = driver.find_elements(by=By.ID, value='search-icon-legacy')
+	btnSubmit[0].send_keys(Keys.ENTER)
+
+	durationSecond = int(input('Duration Second ? > '))
+	durationOpened(durationSecond)
+
+	print('running.....')
+
 
 if __name__ == '__main__':
 	# testing area
-	searchingGoogle = input('Please Search > ')
-	searchGoogle(searchingGoogle)
+	
+
+	# formTextField = driver.find_elements(by=By.NAME, value='q');
+	# formTextField[0].send_keys(command)
+	# time.sleep(3)
+	# btnSubmit = driver.find_elements(by=By.NAME, value='btnK')
+	# btnSubmit[0].send_keys(Keys.ENTER)
+
 
 	# Show My Method 
 	print()
 	welcome()
-	
+
 	while True:
 		menu()
 		ask = input('choose > ')
-		
+
 		if ask == "1":
 			print('Opened Google...')
-			time.sleep(3)
 			print('waiting... ')
-			time.sleep(2)
-			searchingGoogle = input('Please Search > ')
-			searchGoogle(searchingGoogle)
-			print('Result Search > ', searchingGoogle)
-
+			time.sleep(1)
+			searchGoogle = input('Please Search > ')
+			print('Result Search = ', searchGoogle)
+			
+			searchGoogle(searchGoogle)
+			
 		elif ask == "2":
-			print("coming soon")
+			print('Opened Youtube...')
+			print('waiting... ')
+			time.sleep(1)
+			searchYoutube = input('Please Search > ')
+			print('Result Search = ', searchYoutube)
+			
+			searchYoutube(searchYoutube)
+
 			break
 		elif ask == "3":
 			print("coming soon")
 			quit(1)
+		elif ask == "4":
+			print('========== WELCOME ==========')
+			print('')
+
 		else:
 			print('nothing')
